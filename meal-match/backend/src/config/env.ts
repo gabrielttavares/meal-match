54e2d0e2a62ebd36315804cd1da9293e404e42de
unknown <gabrieltxtavares@gmail.com>
2024-11-23 19:19:35 -0300

    Add GOOGLE_CALENDAR_API_KEY (still to be setup)

--- meal-match/backend/src/config/env.ts
+++ meal-match/backend/src/config/env.ts
@@ -2,7 +2,14 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const requiredVariables = ['MONGO_URI', 'SPOONACULAR_API_KEY', 'PORT', 'CHATTGPT_API_KEY', 'YOUTUBE_API_KEY'];
+const requiredVariables = [
+    'MONGO_URI',
+    'SPOONACULAR_API_KEY',
+    'PORT',
+    'CHATTGPT_API_KEY',
+    'YOUTUBE_API_KEY',
+    'GOOGLE_CALENDAR_API_KEY'
+];
 requiredVariables.forEach((variable) => {
     if (!process.env[variable]) {
         throw new Error(`Environment variable ${variable} is not set`);
@@ -14,3 +21,4 @@ export const SPOONACULAR_API_KEY = process.env.SPOONACULAR_API_KEY as string;
 export const PORT = parseInt(process.env.PORT || '5000', 10);
 export const CHATTGPT_API_KEY = process.env.CHATTGPT_API_KEY as string;
 export const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY as string;
+export const GOOGLE_CALENDAR_API_KEY = process.env.GOOGLE_CALENDAR_API_KEY as string;
